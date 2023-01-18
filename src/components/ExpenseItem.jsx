ece80d582b323b8320593bbd378197106243605f
pentiumKing <ramahawai87@gmail.com>
2023-01-17 20:06:25 -0500

    Added a new feature

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 const ExpenseItem = (props) => {
-  
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div className="flex flex-wrap items-center lg:justify-between bg-gray-700 w-full rounded-xl  mt-14 p-2">
       <div>
-        <div>{}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
       <h2 className="text-white font-bold lg:text-3xl text-xl  ml-4">
         {props.title}
