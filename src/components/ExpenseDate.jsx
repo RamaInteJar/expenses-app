3c15bc93b4a219e9549d919d178281247cb61b11
pentiumKing <ramahawai87@gmail.com>
2023-01-17 21:03:37 -0500

    formatted

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
+
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
   const year = props.date.getFullYear();
+
   return (
     <div className="bg-black  text-center text-orange-600 border-purple-800 border-2 rounded-lg w-32 h-24 p-2">
       <div>{month}</div>
