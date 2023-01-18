0dda9207516991f04fac667d9b6ed4afe51cb66a
pentiumKing <ramahawai87@gmail.com>
2023-01-18 16:41:06 -0500

    changed document format

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,26 +1,30 @@
-import React, {useState} from 'react'
-import ExpenseItem from './ExpenseItem';
-import Card from './Card';
-import ExpenseFilter from './ExpenseFilter';
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020")
-  
-  const changeFilterHandler = (selectedYear) => {
-    setFilteredYear(selectedYear)
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  }
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card >
-      <ExpenseFilter selected={filteredYear} onChangeFilter={changeFilterHandler} />
-      {props.spending.map(spendings =>
+    <Card>
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+
+      {props.spending.map((spendings) => (
         <ExpenseItem
           title={spendings.title}
           amount={spendings.amount}
-          date={spendings.date} />)}
-      </Card>
-
+          date={spendings.date}
+        />
+      ))}
+    </Card>
   );
-}
+};
 
-export default Expenses
+export default Expenses;
