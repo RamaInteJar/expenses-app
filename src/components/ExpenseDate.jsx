e846389ec74a04e7c1b42fb8ccba79c9d0c11a7c
pentiumKing <ramahawai87@gmail.com>
2023-01-17 20:41:43 -0500

    adde and remove features

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const ExpenseDate = () => {
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
-    <div>ExpenseDate</div>
-  )
-}
+    <div className="bg-black  text-center text-orange-600 border-purple-800 border-2 rounded-lg w-32 h-24 p-2">
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
+    </div>
+  );
+};
 
-export default ExpenseDate
+export default ExpenseDate;
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,22 +1,17 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   return (
     <div className="flex flex-wrap items-center lg:justify-between bg-gray-700 w-full rounded-xl  mt-14 p-2">
+      <ExpenseDate className="bg-black  text-center text-orange-600 border-purple-800 border-2 rounded-lg w-32 h-24 p-2" />
       <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-      <h2 className="text-white font-bold lg:text-3xl text-xl  ml-4">
-        {props.title}
-      </h2>
-      <div className="bg-purple-900 w-32  text-white border-black border-2 text-center rounded-lg h-16 pr-4 pt-4 lg:text-3xl text-md ml-auto">
-        {props.amount}
+        <h2 className="text-white font-bold lg:text-3xl text-xl  ml-4">
+          {props.title}
+        </h2>
+        <div className="bg-purple-900 w-32  text-white border-black border-2 text-center rounded-lg h-16 pr-4 pt-4 lg:text-3xl text-md ml-auto">
+          {props.amount}
+        </div>
       </div>
     </div>
   );
