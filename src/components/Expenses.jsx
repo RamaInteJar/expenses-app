55ed5f057c98cca9d04746c8cbb0239fc470178b
pentiumKing <ramahawai87@gmail.com>
2023-01-18 16:38:39 -0500

    changed the features outlook

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -13,26 +13,11 @@ const Expenses = (props) => {
   return (
     <Card >
       <ExpenseFilter selected={filteredYear} onChangeFilter={changeFilterHandler} />
+      {props.spending.map(spendings =>
         <ExpenseItem
-          date={props.spending[0].date}
-          title={props.spending[0].title}
-          amount={props.spending[0].amount}
-        />
-        <ExpenseItem
-          date={props.spending[1].date}
-          title={props.spending[1].title}
-          amount={props.spending[1].amount}
-        />
-        <ExpenseItem
-          date={props.spending[2].date}
-          title={props.spending[2].title}
-          amount={props.spending[2].amount}
-        />
-        <ExpenseItem
-          date={props.spending[3].date}
-          title={props.spending[3].title}
-          amount={props.spending[3].amount}
-        />
+          title={spendings.title}
+          amount={spendings.amount}
+          date={spendings.date} />)}
       </Card>
 
   );
