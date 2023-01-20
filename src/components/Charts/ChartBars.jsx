4f164d7ea853b1a060eab5e9c74867a8a1f5869c
pentiumKing <ramahawai87@gmail.com>
2023-01-20 17:31:16 -0500

    added styling

--- src/components/Charts/ChartBars.jsx
+++ src/components/Charts/ChartBars.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const ChartBars = (props) => {
   let barFillHeight = "0%";
-  if (props.max > 0) {
+  if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
   return (
     <div className="chart-bar">
-      <div className="h-32 w-5 bg-purple-900  rounded-lg ">
-        <div style={{ height: barFillHeight }}></div>
+      <div className="h-32 w-5 bg-blue-300  rounded-lg">
+        <div className="chart-bar_fill" style={{ height: barFillHeight }}></div>
       </div>
       <div className="chart-bar_label">{props.label}</div>
     </div>
